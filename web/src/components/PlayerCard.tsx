873ac544ab8563c580c873a327357c341ccd56e1
Scott Key <scottdkey@gmail.com>
2020-10-12 13:29:01 -0600

    clean up state data from PlayerCard

--- web/src/components/PlayerCard.tsx
+++ web/src/components/PlayerCard.tsx
@@ -1,85 +1,95 @@
 // eslint-disable-next-line
 import React, { useState, useEffect } from "react";
 
-import IMAGE from "./utils/Images";
-import SVG from "./utils/SVG";
+import IMAGE from "../assets/Images";
+import SVG from "../assets/SVG";
 import { PlayerObject, PlayerCardInterface } from "./utils/Interfaces";
 import { usePlayerData } from "./providers/PlayerProvider";
 
-
-
 const PlayerCard = ({ player }: PlayerCardInterface) => {
-  const [tank, setTank] = useState(player.tank);
-  const [dps, setDps] = useState(player.dps);
-  const [healer, setHealer] = useState(player.healer);
+  const [cardPlayer, setCardPlayer] = useState<PlayerObject>(player);
 
   const { updatePlayer, removePlayer } = usePlayerData()!;
 
   const handleLocked = () => {
-    const p = { ...player, locked: !player.locked };
+    const p = { ...cardPlayer, locked: !cardPlayer.locked };
+    setCardPlayer(p);
     updatePlayer(p);
   };
 
   const handleIn = () => {
-    const p = { ...player, in: !player.in };
+    const p = { ...cardPlayer, in: !cardPlayer.in };
+    setCardPlayer(p);
     updatePlayer(p);
   };
 
   const updateRoles = (role: string, newBoolean: boolean) => {
-    const updatedPlayer: PlayerObject = { ...player, [role]: newBoolean };
+    const updatedPlayer: PlayerObject = { ...cardPlayer, [role]: newBoolean };
     updatePlayer(updatedPlayer);
   };
 
-  useEffect(() => {}, [player]);
+  useEffect(() => {
+    setCardPlayer(player);
+  }, [player]);
+  
   return (
-    <div className={player.locked ? "card-locked" : "card"}>
+    <div className={cardPlayer.locked ? "card-locked" : "card"}>
       <div className="card-locked-area" onClick={handleLocked}>
-        {player.locked ? (
+        {cardPlayer.locked ? (
           <SVG.ClosedLock className="image locked" />
         ) : (
           <SVG.OpenLock className="image unlocked" />
         )}
       </div>
       <div className="card-head">
-        <div className="name">{player.name}</div>
+        <div className="name">{cardPlayer.name}</div>
 
         <SVG.Trash
           className="image delete-icon"
-          onClick={() => removePlayer(player.id)}
+          onClick={() => removePlayer(cardPlayer.id)}
         />
       </div>
       <div className="role-selection">
         <div className="card-body">
           <img
-            className={tank ? "image roles_active" : "image roles_inactive"}
+            className={
+              cardPlayer.tank ? "image roles_active" : "image roles_inactive"
+            }
             src={IMAGE.tank}
             alt="tank logo"
             onClick={() => {
-              setTank(!tank);
-              updateRoles("tank", !tank);
+              const tank = !cardPlayer.tank;
+              setCardPlayer({ ...cardPlayer, tank });
+              updateRoles("tank", tank);
             }}
           />
           <img
-            className={healer ? "image roles_active" : "image roles_inactive"}
+            className={
+              cardPlayer.healer ? "image roles_active" : "image roles_inactive"
+            }
             src={IMAGE.healer}
             alt="healer logo"
             onClick={() => {
-              setHealer(!healer);
-              updateRoles("healer", !healer);
+              const healer = !cardPlayer.healer;
+              setCardPlayer({ ...cardPlayer, healer });
+              updateRoles("healer", healer);
             }}
           />
           <img
-            className={dps ? "image roles_active" : "image roles_inactive"}
+            className={
+              cardPlayer.dps ? "image roles_active" : "image roles_inactive"
+            }
             src={IMAGE.dps}
             alt="dps logo"
             onClick={() => {
-              setDps(!dps);
-              updateRoles("dps", !dps);
+              const dps = !cardPlayer.dps;
+              setCardPlayer({ ...cardPlayer, dps });
+              updateRoles("dps", dps);
             }}
           />
           <SVG.Dice
             className={`image ${
-              player.in ? "in-the-roll-active" : "in-the-roll"
+              cardPlayer.in ? "in-the-roll-active" : "in-the-roll"
             }`}
             onClick={handleIn}
           />
