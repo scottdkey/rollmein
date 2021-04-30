093f1307b02093f71084370a5022dacefcd32204
Scott Key <scottdkey@gmail.com>
2021-04-30 15:44:08 -0600

    netfliy test

--- packages/frontend/src/components/ValidRole.tsx
+++ packages/frontend/src/components/ValidRole.tsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line
 import React from "react";
-import { Tooltip } from "antd";
 
 interface ValidRoleInterface {
   roleCount: number;
@@ -22,26 +21,8 @@ const ValidRole = ({
   const isRoleValid = roleCount >= miniumNumber;
 
   return (
-    <Tooltip
-      placement="top"
-      title={
-        isRoleValid
-          ? `There are ${roleCount} ${altImageText}${
-              roleCount > 1 && altImageText !== "DPS" ? "s" : ""
-            } in the roll`
-          : toolTipText
-      }
-    >
-      <div className="role">
-        <img
-          src={image}
-          className={isRoleValid ? "valid-image" : "image"}
-          alt={`${altImageText} logo`}
-        />
-        <div className="role-count">{roleCount}</div>
-        {children}
-      </div>
-    </Tooltip>
+    <>
+    </>
   );
 };
 
