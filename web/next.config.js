d8192d83388d97b8ae037c75f719dfcd93876146
Scott Key <scottdkey@gmail.com>
2021-08-13 20:13:46 -0600

    clean build folder -- I guess lets test monorepo build again!

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,8 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+
+
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   distDir: "out",
