e07921bdd3625bc1efbb89d2e1830f3deaa1fbd9
Scott Key <scottdkey@gmail.com>
2021-08-13 20:22:09 -0600

    trying a build

--- web/next.config.js
+++ web/next.config.js
@@ -2,10 +2,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-
-
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   distDir: "out",
   compress: true,
+  target: "serverless",
 });
