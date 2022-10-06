e7d22691a460b3b1637515aef5f6a64a23323fae
Scott Key <34079828+scottdkey@users.noreply.github.com>
2022-10-05 21:46:55 -0600

    remove one frontend

--- frontend/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const __prod__ = process.env.REACT_APP_NODE_ENV === "Production"
-export const __apiUrl__ =  process.env.REACT_APP_API_URL
--- frontend/src/utils/Helpers.tsx
+++
@@ -1,6 +0,0 @@
-import { usePlayerData } from "../components/providers/PlayerProvider";
-const { updatePlayer, removePlayer } = usePlayerData()!;
-
-import { PlayerObject, RoleLogoImageInterface } from "../types/Interfaces";
-
-
