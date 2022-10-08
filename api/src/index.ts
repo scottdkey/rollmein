56a3c4d5100b8f7ea95c2c648d01d9cebe320430
Scott Key <34079828+scottdkey@users.noreply.github.com>
2022-10-08 00:16:48 -0600

    Staging (#8)
    * preview test
    
    * update Backend.yaml
    
    * Update Backend.yml
    
    * test update
    
    * build file
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * cleanup

--- api/src/index.ts
+++ api/src/index.ts
@@ -20,7 +20,10 @@ app.use(
   })
 )
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 9e359560 (Staging (#8))
 app.use(koaJwt({ secret: config.secretKey, passthrough: true }))
 
 Routers.forEach(({ router, routerName }) => {
